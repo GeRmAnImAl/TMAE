b2353fab65bfecb4c042e0de1369303491503e42
AJ Germani <germani.anthony77@gmail.com>
2023-10-27 14:04:37 -0400

    Add functionality to get active user Implemented a method in the CustomUserDetailsService class to retrieve the athlete object associated with the active user and return it.

--- src/main/java/com/group13/tmae/service/Impl/CustomUserDetailsService.java
+++ src/main/java/com/group13/tmae/service/Impl/CustomUserDetailsService.java
@@ -2,8 +2,10 @@ package com.group13.tmae.service.Impl;
 
 import com.group13.tmae.model.Athlete;
 import com.group13.tmae.repository.AthleteRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -30,5 +32,18 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .password(athlete.getPassword())
                 .authorities(Collections.singleton(authority))
                 .build();
+    }
+
+    /**
+     * Retrieves the currently logged-in {@code Athlete} based on the authentication context.
+     * @return The {@code Athlete} object of the currently logged-in user.
+     * @throws UsernameNotFoundException if no {@code Athlete} is found with the username obtained from the authentication context.
+     */
+    public Athlete getLoggedInUser(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUsername = auth.getName();
+
+        return athleteRepository.findByUserName(loggedInUsername)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
     }
 }
