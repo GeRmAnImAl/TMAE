0d77a7d682e245ac5450fd400411d6aa8f0d0bd5
adw139 <adw139@psu.edu>
2023-11-17 23:13:10 -0500

    Added comments and javadoc to BracketController

--- src/main/java/com/group13/tmae/controller/tournaments/BracketController.java
+++ src/main/java/com/group13/tmae/controller/tournaments/BracketController.java
@@ -1,6 +1,5 @@
 package com.group13.tmae.controller.tournaments;
 
-import com.group13.tmae.model.Athlete;
 import com.group13.tmae.model.Bracket;
 import com.group13.tmae.model.Match;
 import com.group13.tmae.model.Tournament;
@@ -10,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller class for handling requests related to tournament brackets.
+ */
 @Controller
 @RequestMapping("/bracket")
 public class BracketController {
@@ -25,6 +26,13 @@ public class BracketController {
     @Autowired
     public TournamentService tournamentService;
 
+    /**
+     * Displays the tournament bracket for a specified tournament.
+     *
+     * @param tournamentID the ID of the tournament to display
+     * @param model        the model used for adding attributes to the view
+     * @return the view name for the tournament bracket
+     */
     @GetMapping("/{tournamentID}")
     public String showTournament(@PathVariable(value = "tournamentID") Long tournamentID, Model model) {
 
@@ -33,14 +41,17 @@ public class BracketController {
 
         List<List<List<Object>>> bracketInfo = new ArrayList<>();
 
+        // Iterate over each round of the tournament
         for (int roundNum = 1; roundNum <= this.tournamentService.calculateTotalRounds(currentTournament); roundNum++){
 
             List<List<Object>> roundInfo = new ArrayList<>();
 
+            // Iterate over each match in the current round
             for(Match match : currentBracket.getMatches()){
 
                 List<Object> matchInfo = new ArrayList<>();
 
+                // Check if the match is part of the current round
                 if(match.getRoundNumber() == roundNum){
 
                     String firstAthlete = match.getAthlete1().getFirstName() + " " + match.getAthlete1().getLastName();
@@ -48,6 +59,7 @@ public class BracketController {
                     Integer firstAthleteScore = match.getAthlete1Score();
                     Integer secondAthleteScore = match.getAthlete2Score();
 
+                    // Add match information to the round
                     matchInfo.add(currentTournament.getCurrentRoundInfo());
                     matchInfo.add(match.getMatchID());
                     matchInfo.add(firstAthlete);
@@ -59,20 +71,10 @@ public class BracketController {
                 }
             }
 
+            // If no matches found for the round, add placeholder entries
             if(roundInfo.isEmpty()){
 
-                int allParticipants = currentTournament.getAllParticipants().size();
-
-                // Make value of all the participants even.
-                int makeParticpantsEven = (allParticipants % 2 == 0) ? allParticipants : allParticipants + 1;
-
-                // Calculate the number of matches in each round
-                for (int roundCalc = 1; roundCalc < roundNum; roundCalc++) {
-                    makeParticpantsEven /= 2;
-                }
-
-                // Divide by two since half are eliminated
-                int matchesPerRound = makeParticpantsEven / 2;
+                int matchesPerRound = getMatchesPerRound(currentTournament, roundNum);
 
                 for(int futureMatches = 0; futureMatches < matchesPerRound; futureMatches++){
                     List<Object> placeholder = new ArrayList<>();
@@ -91,6 +93,7 @@ public class BracketController {
             bracketInfo.add(roundInfo);
         }
 
+        // Add bracket information to the model for rendering in the view
         model.addAttribute("bracket", bracketInfo);
         model.addAttribute("totalRounds", this.tournamentService.calculateTotalRounds(currentTournament));
         model.addAttribute("currentRound", currentTournament.getCurrentRoundInfo());
@@ -98,11 +101,40 @@ public class BracketController {
         return "bracket";
     }
 
+    /**
+     * Retrieves the number of matches per round for a given tournament and round number.
+     *
+     * @param currentTournament the tournament for which to calculate matches
+     * @param roundNum          the round number for which to calculate matches
+     * @return the number of matches per round
+     */
+    private static int getMatchesPerRound(Tournament currentTournament, int roundNum) {
+        int allParticipants = currentTournament.getAllParticipants().size();
+
+        // Make value of all the participants even.
+        int makeParticipantsEven = (allParticipants % 2 == 0) ? allParticipants : allParticipants + 1;
+
+        // Calculate the number of matches in each round
+        for (int roundCalc = 1; roundCalc < roundNum; roundCalc++) {
+            makeParticipantsEven /= 2;
+        }
+
+        // Divide by two since half are eliminated
+        return makeParticipantsEven / 2;
+    }
+
+    /**
+     * Starts the tournament by generating and saving initial brackets if not already done.
+     *
+     * @param tournamentID the ID of the tournament to start
+     * @return the redirect URL after starting the tournament
+     */
     @GetMapping("/startTournament/{tournamentID}")
     public String startTournament(@PathVariable(value = "tournamentID") Long tournamentID) {
 
         Tournament tournament = this.tournamentService.getTournamentById(tournamentID);
 
+        // Generate and save brackets if not already done
         if (!tournament.isInitialBracketsGenerated()) {
             this.tournamentService.generateAndSaveBrackets(tournament);
         }
