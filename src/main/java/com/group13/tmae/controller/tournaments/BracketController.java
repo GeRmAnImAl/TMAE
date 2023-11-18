95d0cfebaae271e103c0540c5b776d98b12d3a37
adw139 <adw139@psu.edu>
2023-11-17 20:34:13 -0500

    Passing and object list to html for bracket

--- src/main/java/com/group13/tmae/controller/tournaments/BracketController.java
+++ src/main/java/com/group13/tmae/controller/tournaments/BracketController.java
@@ -2,6 +2,7 @@ package com.group13.tmae.controller.tournaments;
 
 import com.group13.tmae.model.Athlete;
 import com.group13.tmae.model.Bracket;
+import com.group13.tmae.model.Match;
 import com.group13.tmae.model.Tournament;
 import com.group13.tmae.service.AthleteService;
 import com.group13.tmae.service.TournamentService;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/bracket")
@@ -26,11 +30,55 @@ public class BracketController {
 
         Tournament currentTournament = this.tournamentService.getTournamentById(tournamentID);
         Bracket currentBracket = currentTournament.getBrackets().get(0);
+
+        List<List<List<Object>>> bracketInfo = new ArrayList<>();
+
+        for (int i = 1; i <= this.tournamentService.calculateTotalRounds(currentTournament); i++){
+
+            List<List<Object>> roundInfo = new ArrayList<>();
+
+            for(Match match : currentBracket.getMatches()){
+
+                List<Object> matchInfo = new ArrayList<>();
+
+                if(match.getRoundNumber() == i){
+
+                    String firstAthlete = match.getAthlete1().getFirstName() + " " + match.getAthlete1().getLastName();
+                    String secondAthlete = match.getAthlete2().getFirstName() + " " + match.getAthlete2().getLastName();
+                    Integer firstAthleteScore = match.getAthlete1Score();
+                    Integer secondAthleteScore = match.getAthlete2Score();
+
+                    matchInfo.add(currentTournament.getCurrentRoundInfo());
+                    matchInfo.add(match.getMatchID());
+                    matchInfo.add(firstAthlete);
+                    matchInfo.add(secondAthlete);
+                    matchInfo.add(firstAthleteScore);
+                    matchInfo.add(secondAthleteScore);
+                }
+                roundInfo.add(matchInfo);
+            }
+
+            if(roundInfo.isEmpty()){
+                List<Object> placeholder = new ArrayList<>();
+
+                placeholder.add(i);
+                placeholder.add(null);
+                placeholder.add("To Be Determined");
+                placeholder.add("To Be Determined");
+                placeholder.add("-");
+                placeholder.add("-");
+
+                roundInfo.add(placeholder);
+            }
+
+            bracketInfo.add(roundInfo);
+        }
+
+        model.addAttribute("bracket", bracketInfo);
         model.addAttribute("totalRounds", this.tournamentService.calculateTotalRounds(currentTournament));
         model.addAttribute("currentRound", currentTournament.getCurrentRoundInfo());
-        model.addAttribute("matches", currentBracket.getMatches());
 
-        return "/bracket";
+        return "bracket";
     }
 
     @GetMapping("/startTournament/{tournamentID}")
